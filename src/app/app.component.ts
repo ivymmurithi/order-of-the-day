a936b51e6e7faef002dc63593006b076c5f8ae66
ivymmurithi <ivymurithi@gmail.com>
2022-03-28 20:20:47 +0300

    add javascript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+declare var addList: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    new addList()
+
+  }
+
   title = 'todo-list';
+
 }
---
+++ src/assets/js/scripts.js
@@ -0,0 +1,3 @@
+function addList() {
+    alert('hello!!')
+}
