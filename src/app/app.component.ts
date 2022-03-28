24db1f8d1e85bb76db8fe154332726b0ca3205a4
ivymmurithi <ivymurithi@gmail.com>
2022-03-28 22:20:09 +0300

    todo functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,20 @@ declare var $: any;
 
 export class AppComponent implements OnInit {
 
+
   ngOnInit(): void {
 
-  $(document).ready(function() {
-    $("#addBtn").click(function(event:any) {
-      event.preventDefault();
-        let userInput = $('#todoInput').val();
+    $(document).ready(function() {
+      $("#addBtn").click(function(event:any) {
+        event.preventDefault();
+          let userInput = $('#todoInput').val();
           $('#todoInput').val('');
-        let btn = `<button class="btn btn-outline-dark listdelete"><i class="fa fa-solid fa-trash"></i></button>`;
-        let completed =`<button class="btn btn-outline-dark listcomplete"><i class="fa fa-solid fa-check"></i></button>`;
-        let displayed = $("#showlists").append("<div><li>" + userInput + " " + btn + " " + completed + "</li></div>");
-        $('#showlists').html(displayed)
-        console.log(displayed)
+          let btn = `<button class="btn btn-outline-dark listdelete"><i class="fa fa-solid fa-trash"></i></button>`;
+          let completed =`<button class="btn btn-outline-dark listcomplete"><i class="fa fa-solid fa-check"></i></button>`;
+          $("#showlists").append("<div><li>" + userInput + " " + btn + " " + completed + "</li></div>");
+          console.log(userInput)
+      });
     });
-  });
 
   }
 
